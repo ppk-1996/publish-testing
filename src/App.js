5789426577b8f6396c25e638dcfe701e992acad7
PPK <pyaephyokyaw@qslogics.com>
2020-12-02 00:12:06 +0630

    testing publish judg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Axios from "axios";
+import React from "react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={publishJudgment}>
+        <button>Publish</button>
+      </form>
     </div>
   );
 }
-
 export default App;
+function publishJudgment(e) {
+  e.preventDefault();
+  var url = "http://localhost:8071/api/judgments/publish/";
+  var query = {
+    userId: 4,
+    userName: "thiha@cjs.com",
+    publishedDate: "2020-12-1",
+    publisherEmail: "thiha@cjs.com",
+    id: 118,
+  };
+  var token =
+    "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0aGloYUBjanMuY29tIiwiZXhwIjozMjg2NDM2ODQzLCJpYXQiOjE2MDY4MjA4NDN9.zQUZ9zwbtiTBh-py687bDq-JCBDup2T5SIztV95jgi_BG_JBHbIxaCuyz5oxLJQhLad1Fl5PzKGWHDGSDUFmMg";
+  Axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  Axios.put(url, query, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-type": "Application/json",
+      item: "thiha@cjs.com",
+    },
+  }).then(
+    (response) => {
+      console.log(response);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+}
